34d142ee75c44c5a6a872330d4b983564814fb5f
Keulen Maia <keulensilva17@gmail.com>
2022-05-31 20:41:36 -0300

    Adicionando atividade do encontro remoto 2 ao Github

---
+++ alunos.js
@@ -0,0 +1,14 @@
+for(let alunos =0; alunos <= 17; alunos++){
+  
+  if(alunos == 0){
+    console.log("Zero: " + alunos);
+  }
+  else{
+    if(alunos % 2 == 0){
+      console.log("Par: " + alunos);
+    }
+    if(alunos % 2 == 1){
+      console.log("Impar: " + alunos);
+    }
+  }
+}
